5d75c91954c6cab9304848b1d36681e600888c26
Paweł Krzeszewski <pawkrz4@st.amu.edu.pl>
2023-11-22 14:39:45 +0100

    wstępnie dodane to co powinno być ale jest compile error

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,9 +1,36 @@
 import React from 'react';
 import logo from './bananki.jpg';
 import './App.css';
+import Navbar from "./components/Navbar/Navbar";
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+} from "react-router-dom";
+import Home from "./pages/home";
+import About from "./pages/krzywa";
+import Blogs from "./pages/galeria";
+import SignUp from "./pages/komputery";
 
 function App() {
   return (
+    <Router>
+            <Navbar />
+            <Routes>
+                <Route exact path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route
+                    path="/contact"
+                    element={<Contact />}
+                />
+                <Route path="/blogs" element={<Blogs />} />
+                <Route
+                    path="/sign-up"
+                    element={<SignUp />}
+                />
+            </Routes>
+        </Router>
+        
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
