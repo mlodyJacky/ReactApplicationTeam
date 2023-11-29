48b7d659f28db6c1596e4ff8d1fd936e42749174
Paweł Krzeszewski <pawkrz4@st.amu.edu.pl>
2023-11-29 11:29:30 +0100

    nie sprawdzalem czy dziala ,ale to powinno byc to

--- my-app/src/pages/komputery.js
+++ my-app/src/pages/komputery.js
@@ -4,10 +4,42 @@ import React from "react";
 
 const Komputery = () => {
 	return (
-		<div>
-			<h1>Jako użytkownik na podstronie "Komputery" chcę widzieć pole tekstowe i przycisk dodaj, który dodaje wpisany tekst do listy</h1>
-			<h1>https://pawkrz.atlassian.net/browse/SCRUM-20?atlOrigin=eyJpIjoiZmYxN2UwMWRkOGY5NGEyYWExZjVkZmM4OWM1ZmUyYjkiLCJwIjoiaiJ9</h1>
-		</div>
+$(document).ready(function() {
+    // Array to store items
+    var items = [];
+
+    // Function to add item to the array and update the list
+    function addItem() {
+        var item = $('#itemInput').val();
+        if (item !== '') {
+            items.push(item);
+            updateList();
+            $('#itemInput').val('');
+        }
+    }
+
+    // Function to update the list of items
+    function updateList() {
+        $('#itemList').empty();
+        for (var i = 0; i < items.length; i++) {
+            var listItem = $('<li>').addClass('list-group-item').text(items[i]);
+            $('#itemList').append(listItem);
+        }
+    }
+
+    // Event listener for Add button click
+    $('#addItemBtn').click(function() {
+        addItem();
+    });
+
+    // Event listener for Enter key press in the textbox
+    $('#itemInput').keypress(function(e) {
+        if (e.which === 13) {
+            addItem();
+        }
+    });
+});
+
 	);
 };
 
