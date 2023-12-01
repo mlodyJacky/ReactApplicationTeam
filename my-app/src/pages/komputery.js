897c1364afe97e5bcd2ff1404496241f9e785930
Paweł Krzeszewski <pawkrz4@st.amu.edu.pl>
2023-12-01 16:02:28 +0100

    z jakiegos powodu nie chce mi uzywac funkcji dodawania do listy

--- my-app/src/pages/komputery.js
+++ my-app/src/pages/komputery.js
@@ -1,46 +1,25 @@
-// Filename - pages/signup.js
-
 import React from "react";
 
 const Komputery = () => {
+    function pushData(){
+        let view = document.getElementById("view");
+        let newTd = document.createElement("li");
+        var inputText = document.getElementById("inputText").value;
+        var node = document.createTextNode(inputText);
+        newTd.appendChild(node);
+        view.appendChild(newTd);
+    };
 	return (
-$(document).ready(function() {
-    // Array to store items
-    var items = [];
+        <div>
+            <input type='text' name="text" id="inputText" />
+            <button onclick="pushData()">add to list</button>
 
-    // Function to add item to the array and update the list
-    function addItem() {
-        var item = $('#itemInput').val();
-        if (item !== '') {
-            items.push(item);
-            updateList();
-            $('#itemInput').val('');
-        }
-    }
-
-    // Function to update the list of items
-    function updateList() {
-        $('#itemList').empty();
-        for (var i = 0; i < items.length; i++) {
-            var listItem = $('<li>').addClass('list-group-item').text(items[i]);
-            $('#itemList').append(listItem);
-        }
-    }
-
-    // Event listener for Add button click
-    $('#addItemBtn').click(function() {
-        addItem();
-    });
-
-    // Event listener for Enter key press in the textbox
-    $('#itemInput').keypress(function(e) {
-        if (e.which === 13) {
-            addItem();
-        }
-    });
-});
-
-	);
-};
+            <li>
+            <ul id="view">
+            </ul>
+            </li>
+        </div>
+        
+    )};
 
 export default Komputery;
