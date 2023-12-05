110bd74af0ec0eb8cae502e29a1a29792f3dc159
Krzysztof <krzmak2@st.amu.edu.pl>
2023-12-05 20:44:21 +0100

    wprowadzone dodawanie 2ch liczb, pozostawione na srodku

--- my-app/src/pages/home.js
+++ my-app/src/pages/home.js
@@ -1,8 +1,18 @@
 // Filename - pages/home.js
-import React from 'react';
+import React, { useState } from 'react';
 import logo from '../bananki.jpg';
 
 const Home = () => {
+  const [suma1, setCount] = useState(0);
+
+  const wypisz = () => {
+    // pobiera wartosci z pol tekstowych do zmiennych
+    const liczba1 = parseFloat(document.getElementById('liczba1').value) || 0;
+    const liczba2 = parseFloat(document.getElementById('liczba2').value) || 0;
+
+    setCount(liczba1 + liczba2);
+  };
+
 	return (
 		<div className="App">
       <header className="App-header">
@@ -67,14 +77,18 @@ const Home = () => {
         Twój ulubiony raper kłamie wciąż na trackach
         Gdzie to Lambo co mu kupił tata?</p>
 
-      <br><br>
-      <p>Podaj 2 liczby:</p>
-      <input type="number" id="liczba1" placeholder="Liczba 1"></input>
-      <h1>+</h1>
-      <input type="number" id="liczba2" placeholder="Liczba 2"></input>
-      <button id="przycisk">Dodaj</button>
+      <br></br>
 
 
+        <p>Podaj 2 liczby:</p>
+        <input type="number" id="liczba1" placeholder="Liczba 1"></input>
+        <h1>+</h1>
+        <input type="number" id="liczba2" placeholder="Liczba 2"></input>
+        <br></br>
+        <br></br>
+        <button id="przycisk" onClick={wypisz}>Dodaj</button>
+        <br></br>
+        <h1>Suma: {suma1}</h1>
       </header>
     </div>
 	);
